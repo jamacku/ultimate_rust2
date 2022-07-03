224c323340ff23d659addb912a077d0555a9edaf
Jan Macku <jamacku@redhat.com>
2022-07-03 09:36:47 +0200

    Documentation exercise - solution

--- exercise/docs/src/lib.rs
+++ exercise/docs/src/lib.rs
@@ -15,8 +15,25 @@
 // - Document the "roundness" field, explaining that it is a percentage
 // - Document the "orangeness" field, explaining that it is a number from 8 to 27
 
+/*!
+A pumpkin is a cultivar of winter squash that is round with smooth, slightly ribbed skin, and is most often deep yellow to orange in coloration.
+The thick shell contains the seeds and pulp. The name is most commonly used for cultivars of Cucurbita pepo, but some cultivars of Cucurbita maxima,
+C. argyrosperma, and C. moschata with similar appearance are also sometimes called "pumpkins".
+
+![Large pumpkin](https://upload.wikimedia.org/wikipedia/commons/thumb/5/5c/FrenchMarketPumpkinsB.jpg/700px-FrenchMarketPumpkinsB.jpg)
+*/
+
+/**
+Big orange thing
+
+# Recipes
+
+Recipes will be coming soon ...
+**/
 pub struct Pumpkin {
+    /// `roundness` is a percentage
     pub roundness: f32,
+    /// `orangeness` is a number from 8 to 27
     pub orangeness: i32,
 }
 
@@ -24,19 +41,24 @@ pub struct Pumpkin {
 // can't be used for pie. :'-(
 
 impl Pumpkin {
+    /// If you smash the pumpkin, it will be gone. Then it can't be used for pie. 😥
     pub fn smash(self) {}
 }
 
 // 5. Document that BURNT_ORANGE is for the "orangeness" field in the Pumpkin struct.
 // - Link to the Pumpkin struct in your description
 
+/// `BURNT_ORANGE` is for `orangeness` field in the [`Pumpkin`] struct.
 pub const BURNT_ORANGE: i32 = 13;
 
 // Challenge: Find the option to pass to `cargo doc` so that documentation for this private item
 // gets generated as well.  Hint: `cargo doc -h` will show you all the relevant options.
 
-/// For internal use only. In fact, this documentation is so private that it won't be generated.
-/// At least not by default. But if you pass the correct option in, it will magically appear!
+/**
+For internal use only. In fact, this documentation is so private that it won't be generated.
+At least not by default. But if you pass the correct option in, it will magically appear!
+Option: `--document-private-items`
+**/
 #[allow(dead_code)] // to silence the warning
 enum PrivateEnum {
     /// For Halloween. To be lit by candlelight.
